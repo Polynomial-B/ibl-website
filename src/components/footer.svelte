6599f5eb81ed4bed8aac8e488ff9e4f03c9b6f9b
Matt <mphlamb@gmail.com>
2025-11-14 13:19:49 +0100

    footer added

---
+++ src/components/footer.svelte
@@ -0,0 +1,196 @@
+<script>
+	import Logo from './logo.svelte';
+</script>
+
+<footer>
+	<h2>Stay up to date with our latest offers</h2>
+
+	<div class="subscribe">
+		<input type="email" placeholder="Your email" />
+		<button>SUBSCRIBE</button>
+	</div>
+
+	<div class="icons">
+		<a class="icon" href="/" aria-label="Follow us on Instagram"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="48"
+				height="48"
+				viewBox="0 0 48 48"
+				fill="none"
+			>
+				<circle
+					opacity="0.5"
+					cx="23.695"
+					cy="23.695"
+					r="23.1149"
+					stroke="#645242"
+					stroke-width="1.16039"
+				/>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M23.6932 15.9883C21.6001 15.9883 21.3394 15.9973 20.5162 16.0358C17.7168 16.1642 16.1629 17.7168 16.0345 20.5162C15.9973 21.3394 15.9883 21.6014 15.9883 23.6932C15.9883 25.7864 15.9973 26.0484 16.0345 26.8703C16.1629 29.6685 17.7168 31.2236 20.5162 31.352C21.3394 31.3892 21.6001 31.3982 23.6932 31.3982C25.7864 31.3982 26.0484 31.3892 26.8715 31.352C29.6659 31.2236 31.2261 29.671 31.352 26.8703C31.3892 26.0484 31.3982 25.7864 31.3982 23.6932C31.3982 21.6014 31.3892 21.3394 31.352 20.5162C31.2261 17.7206 29.671 16.1629 26.8715 16.0358C26.0484 15.9973 25.7864 15.9883 23.6932 15.9883ZM23.6898 17.3789C25.7471 17.3789 25.9911 17.3866 26.8039 17.4239C28.8933 17.5189 29.8667 18.509 29.963 20.5816C29.9989 21.3945 30.0066 21.6372 30.0066 23.6944C30.0066 25.7516 29.9989 25.9956 29.963 26.8072C29.8667 28.8786 28.8945 29.8712 26.8039 29.9663C25.9911 30.0022 25.7483 30.0112 23.6898 30.0112C21.6326 30.0112 21.3886 30.0035 20.577 29.9663C18.4838 29.8699 17.5143 28.876 17.418 26.8072C17.382 25.9956 17.373 25.7516 17.373 23.6944C17.373 21.6372 17.382 21.3932 17.418 20.5816C17.513 18.5077 18.4864 17.5176 20.577 17.4226C21.3886 17.3853 21.6326 17.3789 23.6898 17.3789ZM19.7358 23.6938C19.7358 21.5082 21.508 19.7373 23.6923 19.7373C25.8767 19.7373 27.6488 21.5094 27.6488 23.6938C27.6488 25.8794 25.8767 27.6503 23.6923 27.6503C21.508 27.6503 19.7358 25.8794 19.7358 23.6938ZM23.6954 26.2619C22.2765 26.2619 21.1271 25.1125 21.1271 23.6935C21.1271 22.2758 22.2765 21.1252 23.6954 21.1252C25.1132 21.1252 26.2651 22.2745 26.2651 23.6935C26.2651 25.1125 25.1132 26.2619 23.6954 26.2619ZM26.8838 19.5808C26.8838 19.0698 27.2986 18.6562 27.8084 18.6562C28.3208 18.6562 28.7343 19.0698 28.7343 19.5808C28.7343 20.0919 28.3195 20.5054 27.8084 20.5054C27.2973 20.5054 26.8838 20.0907 26.8838 19.5808Z"
+					fill="#645242"
+				/>
+			</svg></a
+		>
+		<a class="icon" href="/" aria-label="Email us"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="48"
+				height="48"
+				viewBox="0 0 48 48"
+				fill="none"
+			>
+				<circle opacity="0.5" cx="24" cy="24" r="23.1149" stroke="#635142" stroke-width="1.16039" />
+				<g transform="translate(14, 15)">
+					<g clip-path="url(#clip0)">
+						<path
+							d="M17.4905 15.7917H1.93719C0.944174 15.7917 0.142578 15.0373 0.142578 14.1027V3.96835C0.142578 3.03374 0.944174 2.2793 1.93719 2.2793H17.4905C18.4836 2.2793 19.2852 3.03374 19.2852 3.96835V14.1027C19.2852 15.0373 18.4836 15.7917 17.4905 15.7917ZM1.93719 3.40533C1.6022 3.40533 1.33899 3.65306 1.33899 3.96835V14.1027C1.33899 14.4179 1.6022 14.6657 1.93719 14.6657H17.4905C17.8255 14.6657 18.0887 14.4179 18.0887 14.1027V3.96835C18.0887 3.65306 17.8255 3.40533 17.4905 3.40533H1.93719Z"
+							fill="#645242"
+						/>
+						<path
+							d="M9.72363 10.908C8.89201 10.908 8.13167 10.5949 7.56141 10.0246L1.32424 3.78534C1.09851 3.56171 1.11039 3.20391 1.348 2.99146C1.5856 2.77901 1.96577 2.79019 2.1915 3.01382L8.42868 9.25309C9.10585 9.93516 10.3414 9.93516 11.0186 9.25309L17.2558 3.025C17.4815 2.80137 17.8617 2.79019 18.0993 3.00264C18.3369 3.21509 18.3488 3.57289 18.123 3.79652L11.8859 10.0358C11.3156 10.6061 10.5553 10.9191 9.72363 10.9191V10.908Z"
+							fill="#645242"
+						/>
+					</g>
+				</g>
+				<defs>
+					<clipPath id="clip0">
+						<rect width="19.9528" height="17.4587" fill="white" />
+					</clipPath>
+				</defs>
+			</svg>
+		</a>
+		<a class="icon" href="/" aria-label="Follow us on Facebook"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="48"
+				height="48"
+				viewBox="0 0 48 48"
+				fill="none"
+			>
+				<circle
+					opacity="0.5"
+					cx="23.695"
+					cy="23.695"
+					r="23.1149"
+					stroke="#635142"
+					stroke-width="1.16039"
+				/>
+				<path
+					d="M21.4472 21.1249H18.8789V23.6932H21.4472V31.3982H25.2997V23.6932H27.6369L27.868 21.1249H25.2997V20.0552C25.2997 19.4414 25.423 19.1987 26.0163 19.1987H27.868V15.9883H24.7809C22.472 15.9883 21.4472 17.0053 21.4472 18.9521V21.1249Z"
+					fill="#645242"
+				/>
+			</svg></a
+		>
+	</div>
+
+	<div class="links">
+		<a href="/about">About Us</a>
+		<a href="/contact">Contact</a>
+		<a href="/terms-and-conditions">Terms & Conditions</a>
+	</div>
+
+	<Logo />
+
+	<p class="copyright">Copyright <span>Insidebeautylondon</span>, All Rights Reserved</p>
+</footer>
+
+<style>
+	h2 {
+		font-family: 'Noto Serif Display', serif;
+		letter-spacing: -0.44px;
+		color: #5b4c3f;
+		text-align: center;
+		font-size: 20px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+		letter-spacing: -1px;
+	}
+
+	footer {
+		width: 100%;
+		padding: 40px 20px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-evenly;
+		text-align: center;
+		gap: 30px;
+	}
+
+	.subscribe {
+		color: #819e80;
+		font-weight: 600;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		width: 250px;
+	}
+
+	.subscribe input {
+		border: 1px solid #819e80;
+		padding: 10px;
+		font-size: 1rem;
+		font-family: 'Raleway', sans-serif;
+		color: #bbb;
+		letter-spacing: -0.255px;
+		font-size: 15px;
+		font-style: normal;
+		font-weight: 500;
+	}
+
+	.subscribe button {
+		padding: 10px;
+		cursor: pointer;
+		font-size: 1rem;
+		border: 1px solid #819e80;
+		background: white;
+		transition: 0.2s ease;
+		width: 137px;
+		margin: 0 auto;
+		font-size: 15px;
+		font-weight: 700;
+		line-height: normal;
+	}
+
+	.subscribe button:hover {
+		background: #f1f1f1;
+	}
+
+	.icons {
+		display: flex;
+		gap: 20px;
+		font-size: 1.2rem;
+	}
+
+	.links {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 20px;
+		color: #645242;
+		text-align: center;
+		font-family: 'Poppins', serif;
+		font-size: 13.697px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: 60%;
+	}
+
+	.copyright {
+		color: #645242;
+		font-family: 'Poppins', sans-serif;
+		font-size: 13.697px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: 120%;
+	}
+
+	.copyright span {
+		font-weight: bold;
+	}
+</style>
