375f77dfbdc1aa808fd33b8698561a8c57763ac5
Matt <mphlamb@gmail.com>
2025-11-15 13:53:01 +0100

    updated styles

---
+++ src/components/imagebody.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	let { src, alt } = $props();
+</script>
+
+<section class="img">
+	<button class="book">MAKE AN APPOINTMENT</button>
+	<img class="main" {src} {alt} />
+	<img class="bg" src="src/public/images/dotted-semi-circle.png" alt="dotted semicircle" />
+</section>
+
+<style>
+	.img {
+		position: relative;
+		text-align: center;
+		padding: 20px 0 60px;
+	}
+
+	.book {
+		display: inline-block;
+		color: white;
+		padding: 10px 20px;
+		border: 2px solid #2ecc71;
+		cursor: pointer;
+		margin-bottom: 20px;
+		font-size: 15px;
+		font-weight: 600;
+		border: 1px solid #fff;
+		background: #819e80;
+		box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.17);
+		margin-bottom: 40px;
+	}
+
+	.main {
+		display: block;
+		margin: 0 auto;
+		position: relative;
+		z-index: 2;
+	}
+
+	.bg {
+		position: absolute;
+		left: 0;
+		bottom: -50px;
+		z-index: 1;
+		pointer-events: none;
+		left: 50vw;
+		transform: translateX(-55vw);
+		z-index: 1;
+	}
+</style>
---
+++ src/components/splash.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let { src, alt } = $props();
+</script>
+
+<div class="splash">
+	<img class="main" {src} {alt} />
+	<img class="bg" src="src/public/images/dotted-circle.png" alt="dotted brown circle underlay" />
+</div>
+
+<style>
+	.splash {
+		padding: 50px 10px;
+		position: relative;
+		height: 550px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.splash img.main {
+		position: relative;
+		z-index: 2;
+		height: 439px;
+	}
+
+	.splash img.bg {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		z-index: 1;
+		pointer-events: none;
+	}
+</style>
