c32e5ef0c66903e3ed83a9fea3abcf05ba77b923
Polynomial-B <mphlamb@gmail.com>
2026-05-31 20:44:42 +0200

    favicon updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import Footer from '../components/footer.svelte';
 	import NavbarDesktop from '../components/navbar-desktop.svelte';
 	import NavbarMobile from '../components/navbar-mobile.svelte';
